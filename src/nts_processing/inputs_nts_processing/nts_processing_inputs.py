cef2cda47cf8a4ebdc7702fe69066be7832d87fb
AdilZ16 <113607872+AdilZ16@users.noreply.github.com>
2024-07-18 14:35:14 +0100

    Production weights model commit
    Model works and is ready for review

--- src/nts_processing/inputs_nts_processing/nts_processing_inputs.py
+++ src/nts_processing/inputs_nts_processing/nts_processing_inputs.py
@@ -30,3 +30,15 @@ class NTS_mts_inputs_AZ_model(BaseConfig):
 
 class NTS_production_weights_inputs(BaseConfig):
     data: Optional[Path] = None
+    data_skip_cb_generation: Optional[Path] = None
+    mode: Optional[List[str]] = None
+    geo_incl: Optional[str] = None
+    segments_incl: Optional[List[str]] = None
+    columns_to_keep: Optional[List[str]] = None
+    output_folder: Optional[Path] = None
+    target_column: Optional[str] = None
+    numerical_features: Optional[List[str]] = None
+    categorical_features: Optional[List[str]] = None
+    index_columns: Optional[List[str]] = None
+    drop_columns: Optional[List[str]] = None
+    ignore_columns: Optional[List[str]] = None
